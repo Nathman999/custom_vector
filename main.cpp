b993203745f593d1a737d7bb7b49fa5263ceaf2e
Nathman <nathman9999@gmail.com>
2020-12-30 19:53:08 +0200

    fix

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <memory>
 
 template<typename T, class Allocator = std::allocator<T>>
 class custom_vector {
@@ -7,10 +8,17 @@ private:
     size_t size = 0;
     T *v = nullptr;
     Allocator Alloc;
+    void construct_vector(T* first){
+        T *last = first + size;
+        while(first != last) {
+            Alloc.construct(first++); // use std::construct_at(first++); if your compiler has -std=c++20 flag
+        }
+    }
 public:
     custom_vector<T, Allocator>(const size_t &size) {
         this->size = size;
         this->v = Alloc.allocate(size);
+        construct_vector(v);
     }
     size_t get_size() {
         return size;
@@ -32,10 +40,16 @@ public:
     void add(T arg) {
         size++;
         T *new_v = Alloc.allocate(size);
+        construct_vector(new_v);
         std::move(v, v + size - 1, new_v);
         Alloc.deallocate(v, size - 1);
         v = new_v;
         *(new_v + size - 1) = arg;
+    }
+
+    void test() {
+        *(v + 1) = "TEST1";
+        std::cout << "TEST" << "\n";
     }
 };
 
@@ -50,15 +64,17 @@ int main() {
         std::cout << v1[i] << "\n";
     }
 
-    //TEST <string>
+    // //TEST <string>
     custom_vector<std::string> v2(3);
     std::string str = "hello world";
+    v2.test();
+
     for(int i = 0; i < v2.get_size(); i++) {
         v2.at(i) = str;
     }
     v2.add("bye world");
     for(int i = 0; i < v2.get_size(); i++) {
-            std::cout<<v2[i] <<"\n";
+        std::cout << v2[i] << "\n";
     }
 }
 
